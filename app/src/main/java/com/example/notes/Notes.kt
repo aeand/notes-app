c121c5c6b5f2a5cf4aa082f81aa90a44f67b350e
aeand <aeand2@gmail.com>
2025-03-06 19:36:22 +0100

    Moved Title down further

--- app/src/main/java/com/example/notes/Notes.kt
+++ app/src/main/java/com/example/notes/Notes.kt
@@ -1,13 +1,15 @@
 package com.example.notes
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.BasicTextField
@@ -15,6 +17,7 @@ import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text.selection.LocalTextSelectionColors
 import androidx.compose.foundation.text.selection.TextSelectionColors
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -73,8 +76,7 @@ fun Notes(
                         if (it.isFocused) {
                             textFieldFocused.value = true
                         }
-                    }
-                    .background(Color.Black),
+                    },
                 value = fileManager.currentFile.content.value,
                 onValueChange = { it: String ->
                     fileManager.currentFile.content.value = it
@@ -136,124 +138,140 @@ fun Notes(
                 minLines = 1,
             )
 
-            BasicTextField(
+            Column(
                 modifier = Modifier
                     .align(Alignment.BottomCenter)
-                    .padding(bottom = 50.dp)
-                    .fillMaxWidth()
-                    .height(70.dp)
-                    .focusRequester(focusRequester)
-                    .onFocusChanged {
-                        if (it.isFocused) {
-                            textFieldFocused.value = true
-                        }
-                    }
-                    .background(Color.Black),
-                value = fileManager.currentFile.title.value,
-                onValueChange = { it: String ->
-                    fileManager.currentFile.title.value = it
-                },
-                cursorBrush = Brush.verticalGradient(
-                    0.00f to Color.White,
-                    0.15f to Color.White,
-                    0.15f to Color.White,
-                    0.75f to Color.White,
-                    0.75f to Color.White,
-                    1.00f to Color.White,
-                ),
-                textStyle = TextStyle(
-                    textAlign = TextAlign.Start,
-                    fontFamily = Typography.titleMedium.fontFamily,
-                    fontSize = Typography.titleMedium.fontSize,
-                    lineHeight = Typography.titleMedium.lineHeight,
-                    letterSpacing = Typography.titleMedium.letterSpacing,
-                    color = Color.White,
-                ),
-                keyboardOptions = KeyboardOptions(
-                    capitalization = KeyboardCapitalization.None,
-                    autoCorrectEnabled = false,
-                    imeAction = ImeAction.Done
-                ),
-                keyboardActions = KeyboardActions(
-                    onDone = {
-                        focusManager.clearFocus()
-                        textFieldFocused.value = false
-                    }
-                ),
-                singleLine = false,
-                maxLines = 1,
-                visualTransformation = VisualTransformation.None,
-                decorationBox = { innerTextField ->
-                    Box(
+            ) {
+                HorizontalDivider(color = Color.DarkGray)
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(70.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.Bottom
+                ) {
+                    BasicTextField(
                         modifier = Modifier
-                            .fillMaxSize()
-                            .padding(start = 20.dp, end = 20.dp)
-                    ) {
-                        if (fileManager.currentFile.title.value.isEmpty()) {
-                            Text(
-                                modifier = Modifier
-                                    .align(Alignment.CenterStart),
-                                text = "Title",
-                                textAlign = TextAlign.Left,
-                                fontFamily = FontFamily(roboto["italic"]!!),
-                                fontSize = Typography.titleMedium.fontSize,
-                                fontWeight = Typography.titleMedium.fontWeight,
-                                lineHeight = Typography.titleMedium.lineHeight,
-                                color = Color.Gray
-                            )
-                        } else {
+                            .fillMaxWidth(0.7f)
+                            .fillMaxHeight()
+                            .focusRequester(focusRequester)
+                            .onFocusChanged {
+                                if (it.isFocused) {
+                                    textFieldFocused.value = true
+                                }
+                            },
+                        value = fileManager.currentFile.title.value,
+                        onValueChange = { it: String ->
+                            fileManager.currentFile.title.value = it
+                        },
+                        cursorBrush = Brush.verticalGradient(
+                            0.00f to Color.White,
+                            0.15f to Color.White,
+                            0.15f to Color.White,
+                            0.75f to Color.White,
+                            0.75f to Color.White,
+                            1.00f to Color.White,
+                        ),
+                        textStyle = TextStyle(
+                            textAlign = TextAlign.Start,
+                            fontFamily = Typography.titleMedium.fontFamily,
+                            fontSize = Typography.titleMedium.fontSize,
+                            lineHeight = Typography.titleMedium.lineHeight,
+                            letterSpacing = Typography.titleMedium.letterSpacing,
+                            color = Color.White,
+                        ),
+                        keyboardOptions = KeyboardOptions(
+                            capitalization = KeyboardCapitalization.None,
+                            autoCorrectEnabled = false,
+                            imeAction = ImeAction.Done
+                        ),
+                        keyboardActions = KeyboardActions(
+                            onDone = {
+                                focusManager.clearFocus()
+                                textFieldFocused.value = false
+                            }
+                        ),
+                        singleLine = false,
+                        maxLines = 1,
+                        visualTransformation = VisualTransformation.None,
+                        decorationBox = { innerTextField ->
                             Box(
                                 modifier = Modifier
-                                    .align(Alignment.CenterStart)
+                                    .fillMaxSize()
+                                    .padding(start = 20.dp, end = 20.dp)
                             ) {
-                                innerTextField()
+                                if (fileManager.currentFile.title.value.isEmpty()) {
+                                    Text(
+                                        modifier = Modifier
+                                            .align(Alignment.CenterStart),
+                                        text = "Title",
+                                        textAlign = TextAlign.Left,
+                                        fontFamily = FontFamily(roboto["italic"]!!),
+                                        fontSize = Typography.titleMedium.fontSize,
+                                        fontWeight = Typography.titleMedium.fontWeight,
+                                        lineHeight = Typography.titleMedium.lineHeight,
+                                        color = Color.Gray
+                                    )
+                                } else {
+                                    Box(
+                                        modifier = Modifier
+                                            .align(Alignment.CenterStart)
+                                    ) {
+                                        innerTextField()
+                                    }
+                                }
                             }
-                        }
+                        },
+                        onTextLayout = {},
+                        interactionSource = interactionSourceTitle,
+                        minLines = 1,
+                    )
+
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .fillMaxHeight(),
+                        horizontalArrangement = Arrangement.End,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            modifier = Modifier
+                                .padding(end = 10.dp)
+                                .clickable {
+                                    focusManager.clearFocus()
+                                    textFieldFocused.value = false
+
+                                    if (fileManager.currentFile.title.value.isNotEmpty() && fileManager.currentFile.content.value.isNotEmpty()) {
+                                        fileManager.saveFile(
+                                            fileManager.currentFile.title.value,
+                                            fileManager.currentFile.path.value,
+                                            fileManager.currentFile.content.value,
+                                            true
+                                        )
+                                    }
+                                },
+                            text = "Save",
+                            color = Color.White,
+                            fontFamily = Typography.bodyLarge.fontFamily,
+                            fontSize = Typography.bodyLarge.fontSize,
+                            fontWeight = Typography.bodyLarge.fontWeight,
+                            lineHeight = Typography.bodyLarge.lineHeight,
+                        )
+
+                        Icon(
+                            modifier = Modifier
+                                .size(50.dp)
+                                .clickable {
+                                    openDir()
+                                },
+                            painter = painterResource(R.drawable.burger_menu),
+                            contentDescription = null,
+                            tint = Color.White
+                        )
                     }
-                },
-                onTextLayout = {},
-                interactionSource = interactionSourceTitle,
-                minLines = 1,
-            )
+                }
+            }
         }
-
-        if (fileManager.currentFile.content.value.isNotEmpty() && fileManager.fileHasChanges()) {
-            Text(
-                modifier = Modifier
-                    .align(Alignment.BottomEnd)
-                    .offset(x = (-60).dp, y = (-11).dp)
-                    .clickable {
-                        focusManager.clearFocus()
-                        textFieldFocused.value = false
-
-                        if (fileManager.currentFile.title.value.isNotEmpty() && fileManager.currentFile.content.value.isNotEmpty()) {
-                            fileManager.saveFile(
-                                fileManager.currentFile.title.value,
-                                fileManager.currentFile.path.value,
-                                fileManager.currentFile.content.value,
-                                true
-                            )
-                        }
-                    },
-                text = "Save",
-                color = Color.White,
-                fontFamily = Typography.bodyLarge.fontFamily,
-                fontSize = Typography.bodyLarge.fontSize,
-                fontWeight = Typography.bodyLarge.fontWeight,
-                lineHeight = Typography.bodyLarge.lineHeight,
-            )
-        }
-
-        Icon(
-            modifier = Modifier
-                .align(Alignment.BottomEnd)
-                .size(50.dp)
-                .clickable {
-                    openDir()
-                },
-            painter = painterResource(R.drawable.burger_menu),
-            contentDescription = null,
-            tint = Color.White
-        )
     }
 }
